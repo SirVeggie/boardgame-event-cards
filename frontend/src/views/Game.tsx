079ac14c6e996a7e88c72def6dd290a29d380762
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-08-08 05:12:06 +0300

    basic game view #deploy

--- frontend/src/views/Game.tsx
+++ frontend/src/views/Game.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { CardType } from 'shared/src/types';
 import { Background } from '../components/Background';
+import { Button } from '../components/Button';
+import { Card } from '../components/Card';
 import { Container } from '../components/Container';
 import { RootState } from '../store';
 import { NotFound } from './NotFound';
@@ -14,12 +18,42 @@ export function Game() {
   const cards = useSelector((state: RootState) => state.cards)
     .filter(card => card.game === game?.name);
 
+  const [usedCards, setUsedCards] = useState([] as CardType[]);
+  const [unusedCards, setUnusedCards] = useState(cards);
+  const [currentCard, setCurrentCard] = useState<CardType | undefined>(undefined);
+
+  useEffect(() => {
+    reset();
+  }, [cards.length]);
+
+  const newCard = () => {
+    const random = Math.floor(Math.random() * unusedCards.length);
+    setCurrentCard(unusedCards[random]);
+    setUsedCards([...usedCards, unusedCards[random]]);
+    setUnusedCards(unusedCards.filter((_, i) => i !== random));
+  };
+  
+  const reset = () => {
+    setCurrentCard(undefined);
+    setUsedCards([]);
+    setUnusedCards(cards);
+  };
+
   if (!game)
     return <NotFound />;
   return (
     <Container className={s.container}>
       <Background bg={game.background} />
       {!cards.length && <div className={s.empty}>Looks empty...</div>}
+      {(currentCard || unusedCards.length) && <div className={s.button} onClick={newCard} />}
+      {currentCard && <Card card={currentCard} />}
+      {!currentCard && unusedCards.length && <div className={s.box}>
+        <h1>Click to draw a card</h1>
+      </div>}
+      {!currentCard && !unusedCards.length && <div className={s.box}>
+        <h1>No more cards</h1>
+        <Button text='Shuffle' onClick={reset} />
+      </div>}
     </Container>
   );
 }
@@ -29,7 +63,32 @@ const useStyles = createUseStyles({
     alignItems: 'center',
     height: '100vh',
   },
-  
+
+  box: {
+    backgroundColor: '#fff',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    padding: '20px',
+
+    '& > h1': {
+      margin: 0,
+    },
+    
+    '& > button': {
+      marginTop: 10,
+    }
+  },
+
+  button: {
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    cursor: 'pointer',
+  },
+
   empty: {
     padding: '1em',
     backgroundColor: '#fff',
