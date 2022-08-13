55f85d25fae45eed13354e6f0c147ba88d8735ca
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-08-13 05:00:22 +0300

    #deploy

--- frontend/src/views/AdvancedGame.tsx
+++ frontend/src/views/AdvancedGame.tsx
@@ -1,11 +1,22 @@
+import { createUseStyles } from 'react-jss';
 import { useGame } from '../hooks/useGame';
+import { useMobile } from '../hooks/useMobile';
+import { usePlayer } from '../hooks/usePlayer';
+import { useSession } from '../hooks/useSession';
 
 export function AdvancedGame() {
   const game = useGame();
+  const player = usePlayer();
+  const session = useSession(player);
+  const mobile = useMobile();
   
   return (
     <div>
-
+      
     </div>
   );
 }
+
+const useStyles = createUseStyles({
+  
+});
